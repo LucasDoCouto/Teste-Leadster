98357c367517e8a5e2bb1a984a80712e68edad1f
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-07-03 13:46:26 -0400

    feat: creating header

---
+++ leadster/src/components/header.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { styled } from "styled-components"
+import Image from 'next/image'
+import { Autour_One } from "next/font/google"
+
+interface HeaderProps {
+
+}
+
+const TagHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 35px 0;
+    background-color: white;
+`
+
+export function Header(props : HeaderProps){
+    return(
+        <TagHeader>
+            <Image
+              src="/logo.png"
+              alt="Leadster Logo"
+              width={194}
+              height={42}
+              priority
+            />
+        </TagHeader>
+    )
+}
